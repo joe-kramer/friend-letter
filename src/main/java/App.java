ff79e47d8d24a5465293f3332ff989a81eda1662
Joe Kramer <joekramer1125@gmail.com>
2017-07-03 10:12:08 -0700

    change /hello to root path and add /favorite_photos route with pictures

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,9 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    get("/hello", (request, response) ->
+    staticFileLocation("/public");
+
+    get("/", (request, response) ->
         "<!DOCTYPE html>" +
          "<html>" +
          "<head>" +
@@ -20,6 +22,23 @@ public class App {
            "<p>Travel Enthusiast Jane</p>" +
          "</body>" +
        "</html>"
+    );
+
+    get("/favorite_photos", (request, response) ->
+    "<!DOCTYPE html>" +
+      "<html>" +
+      "<head>" +
+        "<title>Hello Friend!</title>" +
+        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+      "</head>" +
+      "<body>" +
+       "<h1>Favorite Traveling Photos</h1>" +
+          "<ul>" +
+            "<li><img src='/images/foggymountain.jpeg' alt='A photo of a mountain.'/></li>" +
+            "<li><img src='/images/rockycoast.jpeg' alt='A photo of a a rocky beach.'/></li>" +
+          "</ul>" +
+      "</body>" +
+      "</html>"
     );
   }
 }
